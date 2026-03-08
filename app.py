bdc1dc686d0b9801842f3cd9cfc010fb0bdaf31c
yerrabhumibaby-8106 <yerrabhumibaby@gmail.com>
2026-03-08 23:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ logging.basicConfig(
 # AWS Configuration
 # ----------------------------
 REGION = "ap-south-1"  # Change if needed
-SNS_TOPIC_ARN = "arn:aws:sns:ap-south-1:339713112656:Medtrack"
+SNS_TOPIC_ARN = "arn:aws:iam::117949646235:role/MedTrack"
 
 dynamodb = boto3.resource('dynamodb', region_name=REGION)
 users_table = dynamodb.Table('UsersTable')
